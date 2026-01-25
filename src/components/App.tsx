daeb76be8d5f357f305bb741ae6115b913cad8c8
Skyzary <132926053+Skyzary@users.noreply.github.com>
2026-01-25 15:54:30 +0100

    feat: initialize React Cafe app with voting functionality
    - Add package.json with dependencies and scripts for development
    - Create App component with voting logic and state management
    - Implement CafeInfo component to display cafe details
    - Add VoteOptions component for user voting interface
    - Create VoteStats component to show voting statistics
    - Implement Notification component for no feedback scenario
    - Style components with respective CSS modules
    - Set up TypeScript configuration for the project
    - Configure Vite for development and build processes
    - Add Vercel configuration for deployment

---
+++ src/components/App.tsx
@@ -0,0 +1,51 @@
+import type { VoteType, Votes } from "../types/votes.ts";
+import css from "./App.module.css";
+import CafeInfo from "./CafeInfo.tsx";
+import { useState } from "react";
+import VoteOptions from "./VoteOptions.tsx";
+import VoteStats from "./VoteStats.tsx";
+import Notification from "./Notification.tsx";
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  function handleVotes(VoteType: VoteType) {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [VoteType]: prevVotes[VoteType] + 1,
+    }));
+    console.log(votes);
+  }
+  function resetVotes() {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+    console.log(votes);
+  }
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVotes} onReset={resetVotes} />
+      {totalVotes > 0 && (
+        <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
+      )}
+  
+      {totalVotes === 0 && <Notification />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VoteOptions.tsx
@@ -0,0 +1,24 @@
+import css from "./VoteOptions.module.css";
+import type { VoteType } from "../types/votes.ts";
+interface VoteOptionsProps {
+  onReset: () => void;
+  onVote: (vote: VoteType) => void;
+}
+export default function VoteOptions({ onVote, onReset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
+      <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+        Reset
+      </button>
+    </div>
+  );
+}
